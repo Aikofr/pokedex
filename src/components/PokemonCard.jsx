51e0c20bf9820ee32b76a35bbeea02e7a6ba0cc8
AikoFr <aikootv@gmail.com>
2023-03-28 10:06:23 +0200

    Ajout du Bulbasaur

---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,11 @@
+
+function Bulbi() {
+    return (
+    <figure>
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="photo d'un bulbizar"/>
+        <figcaption>bulbasor</figcaption>
+    </figure>
+    )
+}
+
+export default Bulbi;
