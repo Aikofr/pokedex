1872e920040c112ba5be629dc2a5d7152fcdb057
AikoFr <aikootv@gmail.com>
2023-03-28 16:09:42 +0200

    Retouche React 04

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,7 +1,8 @@
-function PokemonCard(pokemon) {
+function PokemonCard({pokemon}) {
+
     return (
         <figure>
-          {pokemon.img ? (<img src={pokemon.img} alt={pokemon.name}/>) : (<p>???</p>)}
+          {pokemon.imgSrc ? (<img src={pokemon.imgSrc} alt={pokemon.name}/>) : (<p>???</p>)}
           <figcaption>{pokemon.name}</figcaption>
           {/* <h1>{props.name}</h1> */}
         </figure>
