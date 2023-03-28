c4ace9a4e137cab2ef3d66eda81a9e1cca991510
AikoFr <aikootv@gmail.com>
2023-03-28 12:08:47 +0200

    React Basic 03

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
-import Bulbi from "./components/PokemonCard"
+import PokemonCard from "./components/PokemonCard"
 
 function App() {
   return (
     <div>
-      <Bulbi />
+      <PokemonCard />
     </div>
   )
 }
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,11 +1,24 @@
+const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
 
-function Bulbi() {
+function PokemonCard() {
+    const pokemon = pokemonList[0];
+
     return (
-    <figure>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="photo d'un bulbizar"/>
-        <figcaption>bulbasor</figcaption>
-    </figure>
-    )
+        <figure>
+          {pokemon.imgSrc ? (<img src={pokemon.imgSrc} alt={pokemon.name}/>) : (<p>???</p>)}
+          <figcaption>{pokemon.name}</figcaption>
+        </figure>
+    );
 }
 
-export default Bulbi;
+
+export default PokemonCard;
