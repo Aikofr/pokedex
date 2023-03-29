6f02b147e617d3b5d89ad5457c3ad1641f526dc1
AikoFr <aikootv@gmail.com>
2023-03-29 11:01:29 +0200

    React Basics 08

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,23 +1,33 @@
 
-function DisplayButton(props) {
-const {pokemonIndex, setpokemonIndex, pokemonList} = props
+function DisplayButton({pokemonIndex, setpokemonIndex, pokemonList}) {
 
-const handleNext = () => {
-    setpokemonIndex(pokemonIndex + 1)
-}
+// const handleNext = () => {
+//     setpokemonIndex(pokemonIndex + 1)
+// }
 
-const handleBack = () => {
-    setpokemonIndex(pokemonIndex - 1)
-}
+// const handleBack = () => {
+//     setpokemonIndex(pokemonIndex - 1)
+// }
+
+const handleClick = (index) => {
+    setpokemonIndex(index);
+};
 
     return <div>
-        {pokemonIndex > 0 && (
+
+        {pokemonList.map((pokemon, index) => (
+            <button key={pokemon.name} onClick={() => handleClick(index)}>
+                {pokemon.name}
+            </button>
+        ))}
+
+        {/* {pokemonIndex > 0 && (
         <button onClick={handleBack}>prédécent</button>
         )}
         {pokemonIndex < pokemonList.length - 1 && (
         <button onClick={handleNext}>suivant</button>
-        )}
+        )} */}
         </div>
-}
+};
 
 export default DisplayButton;
